34cc8c566701cf1ca80f4bced2b206ab2f42e052
dehacker <robitsegay2018@gmail.com>
2024-01-14 16:47:34 -0800

    mongourl

--- config/MongoDb.js
+++ config/MongoDb.js
@@ -4,14 +4,14 @@ import mongoose from "mongoose";
 const connectDatabase = async () => {
   try {
     mongoose.set('strictQuery', true);
-    // const conn = await mongoose.connect(process.env.MONGOURL, {
-    //   useUnifiedTopology: true,
-    //   useNewUrlParser: true,
-    // });
-    const conn = await mongoose.connect("mongodb://127.0.0.1/desta1pro", {
+    const conn = await mongoose.connect(process.env.MONGOURL, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
+    // const conn = await mongoose.connect("mongodb://127.0.0.1/desta1pro", {
+    //   useUnifiedTopology: true,
+    //   useNewUrlParser: true,
+    // });
 
 
     console.log(`MongoDB Connected`);
